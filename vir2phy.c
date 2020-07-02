23c587c547e27124158ceecdf2aa6bc7bcf3724f
CrisHeredia00 <cherediagalleguillos@gmail.com>
2020-07-02 10:54:12 -0400

    eliminando cambios anteriores

--- vir2phy.c
+++ vir2phy.c
@@ -5,8 +5,7 @@
 
 int main(void) {
     int var = 5;
-    int* x = &var;
-    printf("La direccion virtual en vir2phy de var es: %p \n", x);
+
     printf(1, "el valor de var es: %p\n", getphydir(var));
     exit();
 }
