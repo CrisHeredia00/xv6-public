01d049df3d13544d09045e23cd8c140e8e2b2854
CrisHeredia00 <cherediagalleguillos@gmail.com>
2020-07-02 10:52:25 -0400

    error cprintf

--- vir2phy.c
+++ vir2phy.c
@@ -6,7 +6,7 @@
 int main(void) {
     int var = 5;
     int* x = &var;
-    cprintf("La direccion virtual en vir2phy de var es: %p \n", x);
+    printf("La direccion virtual en vir2phy de var es: %p \n", x);
     printf(1, "el valor de var es: %d\n", getphydir(var));
     exit();
 }
