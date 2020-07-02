9a37b747320b6ca9d1c071ef24a95efc62eef0e1
CrisHeredia00 <cherediagalleguillos@gmail.com>
2020-07-02 10:02:06 -0400

    prueba que funcionen las funciones

--- vir2phy.c
+++ vir2phy.c
@@ -4,6 +4,6 @@
 
 
 int main(void) {
-    printf(1, "La cantidad de procesos en ejecucion en la CPU es %d\n", getphydir(0));
+    printf(1, "La cantidad de procesos en ejecucion en la CPU es %d\n", getphydir(5));
     exit();
 }
