f98242e8ef7a7da5bbcd6246b7803cd4495f7269
CrisHeredia00 <cherediagalleguillos@gmail.com>
2020-07-02 10:55:50 -0400

    arreglar print puntero vir2phy

--- vir2phy.c
+++ vir2phy.c
@@ -5,6 +5,8 @@
 
 int main(void) {
     int var = 5;
+    
+    printf("La direccion virtual en vir2phy de var es: %p \n", (void*)&var);
 
     printf(1, "el valor de var es: %p\n", getphydir(var));
     exit();
