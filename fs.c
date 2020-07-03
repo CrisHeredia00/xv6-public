8cdd94636657b24f149907940e4b6b737c333616
CrisHeredia00 <cherediagalleguillos@gmail.com>
2020-07-03 12:41:06 -0400

    quiza tiene que avanzar uno indirecto...

--- fs.c
+++ fs.c
@@ -396,7 +396,7 @@ bmap(struct inode *ip, uint bn)
     return addr;
   }
 
-  bn -= NINDIRECT; //(le quito uno indirecto)
+  bn += NINDIRECT; //(le quito uno indirecto)
   if(bn < NINDIRECT){
     // Load indirect block, allocating if necessary.
     if((addr = ip->addrs[NDIRECT+1]) == 0)
