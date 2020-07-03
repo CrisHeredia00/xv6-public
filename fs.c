7fadb6b1646aa2f7c0bd837cc90fd275e5e08366
CrisHeredia00 <cherediagalleguillos@gmail.com>
2020-07-03 12:25:53 -0400

    pensando...

--- fs.c
+++ fs.c
@@ -396,11 +396,11 @@ bmap(struct inode *ip, uint bn)
     return addr;
   }
 
-  bn += 1; //(le agrego uno mas para moverme un bloque adelante)
-  if(bn < NINDIRECT+1){
+  bn -= 1; //(le agrego uno mas para moverme un bloque adelante)
+  if(bn < NINDIRECT){
     // Load indirect block, allocating if necessary.
-    if((addr = ip->addrs[NDIRECT-1]) == 0)
-      ip->addrs[NDIRECT-1] = addr = balloc(ip->dev);
+    if((addr = ip->addrs[NDIRECT+1]) == 0)
+      ip->addrs[NDIRECT+1] = addr = balloc(ip->dev);
     bp = bread(ip->dev, addr);
     a = (uint*)bp->data;
     if((addr = a[bn]) == 0){
