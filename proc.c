ac27899e7d28450e0f5caa58b1dd6b2177ea5556
CrisHeredia00 <cherediagalleguillos@gmail.com>
2020-07-08 15:21:50 -0400

    debug2

--- proc.c
+++ proc.c
@@ -537,12 +537,13 @@ int
 phydir(int var){
   //struct proc *curproc = myproc(); //process
   acquire(&ptable.lock);
-  int* pde= PDX(var);
-  //int* pgtab = (pte_t*)P2V(PTE_ADDR(*pde));  //page table
+  int pde= PDX(var);
+  int pgtab = (pte_t*)P2V(PTE_ADDR(pde));  //page table
   //int* pte = PTX(*pgtab); //page table posicion
   //int* variable_adress = &var;
   //int phydir = ((PTE_ADDR(*pte)& 0xFFFFF000) | (((uint)(*variable_adress))&0xFFF));
   //cprintf("%p", phydir);
+  cprintf("%p", pgtab);
   release(&ptable.lock);
   return 0;
 }
